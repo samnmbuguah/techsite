2d54d781f6cd089c140528f2a5c4ab4d656dabfa
Samuel Nyambura <samnmbuguah@gmail.com>
2025-02-24 09:40:23 +0000

    ✨ Add analytics tracking utility functions

---
+++ src/utils/analytics.ts
@@ -0,0 +1,10 @@
+// Analytics tracking functions
+export const trackNewsletterSubscription = () => {
+  // TODO: Implement actual analytics tracking
+  console.log('Newsletter subscription tracked');
+};
+
+export const trackSocialMediaClick = (platform: string) => {
+  // TODO: Implement actual analytics tracking
+  console.log(`Social media click tracked: ${platform}`);
+}; 
