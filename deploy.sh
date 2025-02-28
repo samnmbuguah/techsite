907d6b5d8a533b283f2989aada3fbc17b900af12
Samuel Nyambura <samnmbuguah@gmail.com>
2025-02-28 07:03:50 +0000

    🚀 Update deploy script to include .env file in production deployment

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ cp package-lock.json production/
 cp server.js production/
 cp -r .next production/.next
 cp -r public production/
+cp .env production/
 
 # Use rsync to upload files to the server
 echo "Uploading to server using rsync..."
